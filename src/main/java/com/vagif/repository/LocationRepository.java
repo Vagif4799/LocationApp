d209de63586924eac558f72793430770e73a38b1
vagif4799 <quliyev_vaqif@icloud.com>
2019-12-31 11:07:04 +0400

    -- location service added

--- src/main/java/com/vagif/repository/LocationRepository.java
+++ src/main/java/com/vagif/repository/LocationRepository.java
@@ -1,7 +1,7 @@
 package com.vagif.repository;
 
 import com.vagif.entity.Location;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationRepository extends CrudRepository<Location, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
 }
