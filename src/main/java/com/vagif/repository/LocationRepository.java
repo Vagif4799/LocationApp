074126cfe406aa1f003660fc55f3348b61599371
vagif4799 <quliyev_vaqif@icloud.com>
2019-12-31 10:55:13 +0400

    -- location entity and repo added

---
+++ src/main/java/com/vagif/repository/LocationRepository.java
@@ -0,0 +1,7 @@
+package com.vagif.repository;
+
+import com.vagif.entity.Location;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocationRepository extends CrudRepository<Location, Integer> {
+}
